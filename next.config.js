de85d88520a843661099c6f71ee2a0cb5c0f1a0b
tom <anytable@gmail.com>
2024-06-27 01:15:23 +0800

    1. upgrade to Next 15 Rc 2. add unoCSS 3. add playwright

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+import UnoCSS from '@unocss/webpack'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // To support HMR.
+    config.cache = false
+    config.plugins.push(
+        UnoCSS(),
+    )
+    return config
+  },
+  // @see https://react.dev/learn/react-compiler
+  experimental: {
+    reactCompiler: true,
+  }
 }
 
-module.exports = nextConfig
+export default nextConfig
