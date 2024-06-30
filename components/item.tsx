70aa73229c3a10c89799c67b0f4924e1b337681c
Tom Z <anytable@gmail.com>
2024-06-30 11:46:59 +0800

    plus 1

--- components/item.tsx
+++ components/item.tsx
@@ -4,6 +4,7 @@ import clsx from "clsx";
 import {Item} from "@types";
 import {useCurrency} from "@store";
 import {fmt} from "../utils";
+import { useEffect, useState} from "react";
 
 const {item, gift, bg0, bg1, bg2, bg3, bg4} = t
 
@@ -15,12 +16,48 @@ const selectTheme = (id: string) => {
     }
     return clsx(gift, [bg0, bg1, bg2, bg3, bg4][i % 5]);
 }
+
+const PlusOne = ({id = 1}) => {
+    // you can get colors from https://coolors.co/palettes/trending
+    const colors = [
+        '#006d77', '#83c5be', '#edf6f9', '#ffddd2', '#e29578'
+    ]
+    useEffect(() => {
+        const t = setTimeout(() => {
+            ss(1)
+        }, 30)
+        return () => clearTimeout(t)
+    }, [])
+
+    const style = {color: colors[id % 5]};
+    const [s, ss] = useState(0)
+    return <span className={clsx(s && 'act', t.plus)} style={style}>+1</span>
+}
+
 export const Product = ({data}: {
     data: Item
 }) => {
+    const [plusOnes, setPlus] = useState([] as number[])
     const [currency] = useCurrency()
     const t = selectTheme(data.id)
-    return <div className={item}>
+    const click = () => {
+        const t = Date.now()
+        setPlus(plusOnes.concat(t))
+    }
+
+    useEffect(() => {
+        const t = setTimeout(() => {
+            const n = Date.now() - 1e3
+            const i = plusOnes.findIndex(a => a > n)
+            if (i > 0) {
+                setPlus(plusOnes.slice(0, i))
+            }
+        }, 1e3)
+        return () => clearTimeout(t)
+    }, [plusOnes])
+
+    return <div className={item} onClick={click}>
+        {plusOnes.map((c) => <PlusOne id={c} key={c}/>)}
         <p className={'color-light text-sm-0'}>{data.title}</p>
         <div className={'flex-1 relative bg-amber justify-center items-center'}>
             <Cube
@@ -39,7 +76,7 @@ export const Product = ({data}: {
         </div>
         <p className={'color-amber text-sm-1'}>
             <span>{currency.symbol}</span>
-            <span>{fmt(data.price * currency.usdCoef,currency)}</span>
+            <span>{fmt(data.price * currency.usdCoef, currency)}</span>
         </p>
     </div>
 }
